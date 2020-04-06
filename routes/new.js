7e7a5f972c88d9e6df034b98cc737db021ba86c2
Obylisk <obylisk0gmail.com>
2020-04-06 16:50:55 +0700

    Corrected spelling error

--- routes/new.js
+++ routes/new.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //??????
 /* GET user's info and message. */
 router.get('/', (req, res, next) => {
-  res.send('respong with resource'});
+  res.send('respond with resource'});
 });
 
 module.exports = router;
